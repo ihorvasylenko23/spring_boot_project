1c668f4068b524fa6b76b90a8e7f691ab324382f
Ihor Vasylenko <vasylenkoihor23@gmail.com>
2023-09-08 08:19:24 +0300

    implemented task

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,15 +1,26 @@
 package mate.academy.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-
+    @Size(min = 1, max = 255)
+    @NotNull
     private String title;
+    @NotNull
+    @Size(min = 1, max = 255)
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Positive
     private BigDecimal price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
