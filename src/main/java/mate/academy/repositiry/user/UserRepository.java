d954e4a80422a4eaddf75464490fb078e892a936
Ihor Vasylenko <vasylenkoihor23@gmail.com>
2023-09-26 23:11:23 +0300

    improved solution

--- src/main/java/mate/academy/repositiry/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package mate.academy.repositiry.user;
-
-import java.util.Optional;
-import mate.academy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
